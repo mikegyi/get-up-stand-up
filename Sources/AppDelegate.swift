d4dbe7e69836e875f489a0de70380c92bc5adeac
mikegyi <mike.gyi@hey.com>
2026-03-31 13:19:58 +0200

    fix: prevent duplicate menu bar instances

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -2,6 +2,19 @@ import AppKit
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
+        guard !isDuplicateInstance() else {
+            NSApp.terminate(nil)
+            return
+        }
+
         NSApp.setActivationPolicy(.accessory)
+    }
+
+    private func isDuplicateInstance() -> Bool {
+        guard let bundleIdentifier = Bundle.main.bundleIdentifier else {
+            return false
+        }
+
+        return NSRunningApplication.runningApplications(withBundleIdentifier: bundleIdentifier).count > 1
     }
 }
